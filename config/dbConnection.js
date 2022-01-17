7944e8fd4985b56290fb61fb46c755adff6d31fe
enrique1994mex <enrique.antonio1029@gmail.com>
2022-01-17 12:44:46 -0600

    Se agregaron variables de entorno

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,11 +1,14 @@
 const mysql = require('mysql2');
 
+const dotenv = require('dotenv');
+dotenv.config(); 
+
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'dwjqc_524183', 
-    port: 3306, 
-    database: 'bdinventariolibros'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD, 
+    port: process.env.DB_PORT, 
+    database: process.env.DB_DATABASE
 });
 
 connection.connect((error, result) => {
